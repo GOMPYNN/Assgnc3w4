ffb3a01f5cf0faabbb832b233523f433fb3c90c4
Bert Haak <bert.haak@upcmail.nl>
2016-12-17 16:51:27 +0100

    Data Getting and Cleaning Course Project
    The repository contains the R script, the Codebook, the Readme file and
    the tidy dataset for upload to GitHub.

---
+++ run_analysis.R
@@ -0,0 +1,86 @@
+library(dplyr)
+
+if (!file.exists("./data")){
+        dir.create("./data")
+}
+
+# Download, record date of dowload and unzip the datasets
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl, dest="./data/Dataset.zip", mode="wb")
+
+# Record date of download
+dateDowloaded <- date()
+# dateDowloaded = "Sat Dec 17 16:39:11 2016"
+
+# Unzip the datasets
+unzip ("./data/Dataset.zip", exdir = "./data")
+
+# UCI HAR Dataset directory created by unzip command contains the unzipped files
+# Read the activity labels and the features
+activitylabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
+features <- read.table("./data/UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+
+# Select the mean and standard deviation from the features dataset
+# This is needed for point 2 of the assignment.
+# Search for mean or std preceded or followed by any character any number of times
+featuresNeeded <- grep(".*mean.*|.*std.*", features[, 2])
+featuresNames <- features[featuresSelected, 2]
+
+# Remove "-" and "()" from featuresNames
+featuresNames <- gsub("-","", featuresNames)
+featuresNames <- gsub("\\(+\\)","", featuresNames)
+
+# Load the train datasets
+trainSet <- read.table("./data/UCI HAR Dataset/train/X_train.txt")[featuresNeeded]
+trainLabels <- read.table("./data/UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+# Combine the three sets into one train dataset
+train <- cbind(trainSubjects, trainLabels, trainSet)
+
+# Load the test  datasets
+testSet <- read.table("./data/UCI HAR Dataset/test/X_test.txt")[featuresNeeded]
+testLabels <- read.table("./data/UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+# Combine the three sets into one test dataset
+test <- cbind(testSubjects, testLabels, testSet)
+
+# Merge the train and test data sets
+# This is needed for point 1 of the assignment.
+mergedData <- rbind(train, test)
+
+# Add column names
+# This is needed for point 4 of the assignment
+colnames(mergedData) <- c("subjectid", "activity", featuresNames)
+
+# Replace activity values 1 to 6 for descriptive names
+# This is needed for point 3 of the assignment
+mergedData$activity <- as.character(mergedData$activity)
+mergedData$activity <- gsub("1", "walking", mergedData$activity)
+mergedData$activity <- gsub("2", "walkingupstairs", mergedData$activity)
+mergedData$activity <- gsub("3", "walkingdownstairs", mergedData$activity)
+mergedData$activity <- gsub("4", "sitting", mergedData$activity)
+mergedData$activity <- gsub("5", "standing", mergedData$activity)
+mergedData$activity <- gsub("6", "laying", mergedData$activity)
+
+# mergedData must by grouped by subjectid and activity to calculate means for each variable
+
+
+# This is the independent tidy dataset with means for each variable
+tidy <- mergedData %>% group_by(subjectid, activity) %>%
+        summarize_each(funs(mean))
+
+#Ofcourse, R also supports vectorisation, which can be of particular interest if you are interested in performance. FOr a vectorised solution, we first create a lookup vector:
+        
+#        translator_vector = c(A = 'Text for A',
+#                              B = 'Text for B',
+#                              C = 'Text for C')
+
+# and subset this vector using df$ID:
+        
+        dum_vectorized = translator_vector[df$ID]
+
+# Lastly write the tidy data set to a file in csv format 
+write.csv(tidy, "./data/tidy.csv", row.names = FALSE)
+
